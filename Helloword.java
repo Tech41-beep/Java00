bb2ff745ed71bad1a25c4339aed038f55b5d82e9
LovKimtech <Lovkimtech@gmail.com>
2026-01-12 10:18:46 +0700

    initialized

---
+++ Helloword.java
@@ -0,0 +1,6 @@
+public class Helloword { //main class = static void main
+                             //sout= print statement
+    public static void main(String[] args) {
+       System.out.println("Hello, World!");
+    }
+    }
